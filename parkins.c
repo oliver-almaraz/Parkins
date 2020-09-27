2aeccc137a68b8b2ed7e9b5b9e0a7ebc8610bb12
oliver-almaraz <oliver.almaraz@gmail.com>
2020-09-27 01:16:01 -0500

    VSC

--- parkins.c
+++ parkins.c
@@ -5,25 +5,26 @@
 #include <time.h>
 #include <locale.h>
 
-const char *braille[] = {
+const char *braille[] = { // Todas las combinaciones posibles de seis puntos ordenados según su representación Unicode
 	//  	0 	  1 	2 	 3 	  4    5 	6 	 7     8    9    A     B    C    D    E 	F
 /*U+280x*/ 	"⠀", "⠁", "⠂", "⠃", "⠄", "⠅", "⠆", "⠇", "⠈", "⠉", "⠊", "⠋", "⠌", "⠍", "⠎", "⠏",
 /*U+281x*/ 	"⠐", "⠑", "⠒", "⠓", "⠔", "⠕", "⠖", "⠗", "⠘", "⠙", "⠚", "⠛", "⠜", "⠝", "⠞", "⠟",
 /*U+282x*/ 	"⠠", "⠡", "⠢", "⠣", "⠤", "⠥", "⠦", "⠧", "⠨", "⠩", "⠪", "⠫", "⠬", "⠭", "⠮", "⠯",
 /*U+283x*/ 	"⠰", "⠱", "⠲", "⠳", "⠴", "⠵", "⠶", "⠷", "⠸", "⠹", "⠺", "⠻", "⠼", "⠽", "⠾", "⠿"
 };
-const char *perk[] = {
+const char *perk[] = { // Equivalentes en combinación Perkins (f=punto 1, d=2, s=3, j=4, k=5, l=6) ordenados alfab.
 //	 0	   1	  2	      3	 	 4		5	  	6		 7		 8	    9	    A		 B		 C		 D		   E		 F
 	" ",  "f",   "d",   "df",   "s",   "fs",   "ds",   "dfs",   "j",   "fj",   "dj",   "dfj",   "js",   "fjs",   "djs",   "dfjs",
 	"k",  "fk",  "dk",  "dfk",  "ks",  "fks",  "dks",  "dfks",  "jk",  "fjk",  "djk",  "dfjk",  "jks",  "fjks",  "djks",  "dfjks",
 	"l",  "fl",  "dl",  "dfl",  "ls",  "fls",  "dls",  "dfls",  "jl",  "fjl",  "djl",  "dfjl",  "jls",  "fjls",  "djls",  "dfjls",
 	"kl", "fkl", "dkl", "dfkl", "kls", "fkls", "dkls", "dfkls", "jkl", "fjkl", "djkl", "dfjkl", "jkls", "fjkls", "djkls", "dfjkls"
 };
-const char *alpha[] = {
-			"_", "a", ",", "b", ".", "k",  ";",  "l", "?",  "c", "i", "f", "í", "m",  "s", "p",
-			"@", "e", ":", "h", "*", "o",  "!",  "r", "?",  "d", "j", "g", ")", "n",  "t", "q",
-			"?", "?", "?", "(", "-", "u",  "\"",  "v", "?",  "?", "?", "?", "ó", "x",  "é", "?",
-			"?", "?", "?", "ü", "?", "z",  "=",  "á", "|",  "?", "w", "ñ", "?", "y",  "ú", "?"
+const char *alpha[] = { // Las letras que corresponden a un solo signo. Signos poco comunes,
+// inexistentes en español, o que requieren más de un signo para interpretarse se ignoran ("�").
+	   "\u00A0", "a", ",", "b", ".", "k",  ";",  "l", "�",  "c", "i", "f", "í", "m",  "s", "p",
+			"@", "e", ":", "h", "*", "o",  "!",  "r", "�",  "d", "j", "g", ")", "n",  "t", "q",
+			"�", "�", "?", "(", "-", "u",  "\"", "v", "�",  "�", "�", "�", "ó", "x",  "é", "�",
+			"�", "�", "�", "ü", "?", "z",  "=",  "á", "|",  "�", "w", "ñ", "�", "y",  "ú", "�"
 };
 char *nums[] = {
   // a	b	c	d	 e	  f		g	h	  i	   j
@@ -62,7 +63,10 @@ int main() {
 		
 		fgets(input, 8, stdin);
 
-		// Los saltos de línea y tabulaciones se pasan tal cual
+		/******************************************************
+		 * Los saltos de línea y tabulaciones se pasan tal cual
+		*******************************************************/
+
 		if (! strcmp(input, "\n")) {
 			strcat(textoBrai, "\n");
 			strcat(texto, "\n");
@@ -74,17 +78,12 @@ int main() {
 			NUMERAL = 0;
 			continue;
 		}
-		 else if (! strcmp(input, "b\n") || ! strcmp(input, "B\n")) { // Borrar el útimo elemento
-			if (textoBrai[strlen(textoBrai)-1] == '\n' || textoBrai[strlen(textoBrai)-1] == '\t')
-			// Para borrar tabulación o salto solo se borra el último char
-				textoBrai[strlen(textoBrai)-1] = '\0';
-			else // Caracteres braille requieren borrar 3 chars
-				for (int i=0; i <3; i++)
-					textoBrai[strlen(textoBrai)-1] = '\0';
-			// Se borratambién el texto normal
-			texto[strlen(texto)-1] = '\0';
-		}
-		 else if (! strcmp(input, "r\n") || ! strcmp(input, "R\n")) { // R borra solo de la parte de BRaille
+		
+		/********************************************************
+		 * input == "B" borra el último elemento de ambas partes
+		*********************************************************/
+
+		 else if (! strcmp(input, "b\n") || ! strcmp(input, "B\n")) {
 			if (textoBrai[strlen(textoBrai)-1] == '\n' || textoBrai[strlen(textoBrai)-1] == '\t')
 			// Para borrar tabulación o salto solo se borra el último char
 				textoBrai[strlen(textoBrai)-1] = '\0';
@@ -92,22 +91,38 @@ int main() {
 				for (int i=0; i <3; i++)
 					textoBrai[strlen(textoBrai)-1] = '\0';
 
-		} else if (! strcmp(input, "t\n") || ! strcmp(input, "T\n")) { // T borra solo de la parte de texto
+			// Se borratambién el texto normal
 			texto[strlen(texto)-1] = '\0';
 		}
-		else if (! strcmp(input, "SALIR\n") || ! strcmp(input, "salir\n")) { // Salir
+		 // R borra solo de la parte de Braille
+		 else if (! strcmp(input, "r\n") || ! strcmp(input, "R\n")) {
+			if (textoBrai[strlen(textoBrai)-1] == '\n' || textoBrai[strlen(textoBrai)-1] == '\t')
+			// Para borrar tabulación o salto solo se borra el último char
+				textoBrai[strlen(textoBrai)-1] = '\0';
+			else // Caracteres braille requieren borrar 3 chars
+				for (int i=0; i <3; i++)
+					textoBrai[strlen(textoBrai)-1] = '\0';
+
+		}
+		 // T borra solo de la parte de texto
+		 else if (! strcmp(input, "t\n") || ! strcmp(input, "T\n")) {
+			texto[strlen(texto)-1] = '\0';
+		}
+		 else if (! strcmp(input, "SALIR\n") || ! strcmp(input, "salir\n")) { // Salir
 			free(texto);
 			exit(0);
 		}
 
-		// escribir "w" crea un nuevo archivo y escribe los contenidos en braille en él.
+		/*****************************************************************
+		 * input == "W" crea un documento y guarda el texto braille en él
+		******************************************************************/
+
 		 else if (! strcmp(input, "w\n") || ! strcmp(input, "W\n")) {
 
 			char nombre[25];
 			time_t t = time(NULL);
   			struct tm tm = *localtime(&t);
   			sprintf(nombre,"braille_%02d-%02d_%02d:%02d.txt", tm.tm_mday, tm.tm_mon + 1, tm.tm_hour, tm.tm_min);
-			
 
 			FILE *fptr = fopen(nombre, "w");
 
@@ -131,9 +146,12 @@ int main() {
 				free(texto);
 				exit(0);
 			}
-			else
+			 else
 				continue;
 		}
+		/*************************
+		 * SI ES UNA SIGNO BRAILLE
+		**************************/
 		 else
 			input[strcspn(input, "\n")]  = '\0'; // Se elimina el \n
 
@@ -162,6 +180,10 @@ int main() {
 			continue;
 		}
 
+		/***********************************
+		 * SI ES UNA LETRA O NÚMERO BRAILLE
+		************************************/
+
 		for (int i = 0; i <= 64; i++) {
 			if (! strcmp(input, perk[i])) { // strcmp() retorna 0 si son idénticas
 				// Se añade el signo braille a textoBrai
@@ -187,7 +209,7 @@ int main() {
 						}
 					}
 				} 
-				else {
+				 else {
 					strcat(texto, alpha[i]);
 					continue;
 				}
